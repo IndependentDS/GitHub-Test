507001415255cfa0a69c62339b584b8c1ddf9353
Jason Wyatt <jasonwyatt@jasons-macbook-pro.local>
2018-03-21 07:06:42 -0400

    Initial Commit

---
+++ GitHub-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub-Test
+//
+//  Created by Jason Wyatt on 3/21/18.
+//  Copyright © 2018 Jason Wyatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
